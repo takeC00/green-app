524bb75cc1c40c41c338f7a57f0101c84610d363
takeC00 <take.614@icloud.com>
2023-07-22 14:55:51 +0900

    Route::namespace

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\Sample\SampleController;
 use App\Http\Middleware\HelloMiddleware;
 use Illuminate\Support\Facades\Route;
 
@@ -31,4 +32,9 @@ Route::get('/', function () {
 Route::middleware([HelloMiddleware::class])->group(function () {
     Route::get('/hello', [HelloController::class, 'index']);
     Route::get('hello/other', [HelloController::class, 'other']);
+});
+
+Route::namespace('Sample')->group(function(){
+    Route::get('/sample', [SampleController::class, 'index']);
+    Route::get('/sample/other', [SampleController::class, 'other']);
 });
