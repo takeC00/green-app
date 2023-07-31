71c036ada779f7be6704be7473e8f0cc01119269
takeC00 <take.614@icloud.com>
2023-08-01 08:18:54 +0900

    .env環境変数

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -14,11 +14,11 @@ class HelloController extends Controller
 
     public function index(Person $person)
     {
-        $sample_msg = config('sample.message');
-        $sample_data = config('sample.data');
+        $sample_msg = env("SAMPLE_MESSAGE");
+        $sample_data = env('SAMPLE_DATA');
         $data = [
             'msg' => $sample_msg,
-            'data' => $sample_data
+            'data' => explode(',', $sample_data)
         ];
 
         return view('hello.index', $data);
