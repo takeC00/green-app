db5e1bdfe74bb4dbbb18bc1bfc06f24a8426d1bd
takeC00 <take.614@icloud.com>
2023-08-31 08:38:01 +0900

    特定のレコード取得

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,23 +8,27 @@ use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
-    public function index($id = -1)
+    public function index()
     {
-        if($id >= 0)
-        {
-            $msg = 'get name like"'.$id.'".';
+            $msg = 'get name people records';
 
             //$result = DB::table('people')->where('name','like','%'.$id.'%')->get();
 
             //whereRaw
             //SQLクエリーをパラメータで送られてきた$idと繋ぎ合わせてるので$idが不正な値でないかをチェックする必要がある
-            $result = DB::table('people')->whereRaw('name like "%'.$id.'%"')->get();
-        }
-        else
-        {
-            $msg = 'get people records.';
-            $result = DB::table('people')->get();
-        }
+            //$result = DB::table('people')->whereRaw('name like "%'.$id.'%"')->get();
+
+            //whereRaw(安全ver)
+            //プレースホルダーでパラメータを組み込むように修正
+            //$result = DB::table('people')->whereRaw('name like ?', ['%'.$id.'%'])->get();
+
+            //最初のレコード
+            $first = DB::table('people')->first();
+            //最後のレコード
+            $last = DB::table('people')->orderBy('id', 'desc')->first();
+            $result = [$first, $last];
+
+
         $data = [
             'msg' => $msg,
             'data' => $result,
